b4aa056e9ce231c6370b0763fb5e4b3e79957b04
gkarthik <xraydiffraction@gmail.com>
2018-07-03 17:28:05 -0700

    Mod footer.

--- ui.R
+++ ui.R
@@ -17,9 +17,9 @@ shinyUI(fluidPage(
         plotlyOutput("trendPlot"),
         htmlOutput("detailsText")
     ),
-    ),
     hr(),
-    tags$div(class = "fotter", style="text-align:right;", checked = NA,
+    tags$div(class = "footer", style="text-align:right;", checked = NA,
              tags$a(href = "https://andersen-lab.com/", "Andersen Lab")
              )
+    )
     );
